555aa8a7a936e401f206675f34512a4d8bba1f51
diyarani823 <diyarani132@gmail.com>
2025-09-25 13:23:31 +0500

    ACT 1

---
+++ Code in place/Hands on activity 1- Diya Rani-CT-165.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	
+    float weight, dosage;
+    printf("Enter patient's weight: ");
+    scanf("%f", &weight);
+    dosage = weight * 0.8;
+    printf("The dosage(mg) for the patient is: %.2f\n", dosage);
+    return 0;
+}
+
