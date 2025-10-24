f72f9b8cb4aae058590b6df39c3ed7019d8a5af9
diyarani823 <diyarani132@gmail.com>
2025-10-24 16:52:40 +0500

    labs

---
+++ Code in place/Hands on activity 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+ {
+    float watts, kilowatts;
+    printf("Enter power in watts: ");
+    scanf("%f", &watts);
+    kilowatts = watts / 1000.0;
+    printf("The power in kilowatts is: %.2f\n", kilowatts);
+    return 0;
+}
+
---
+++ Labs/Lab 1 task 2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Name:\tDiya\nDepartment:\tCSIT\n");
+    printf("1st semester\n");
+    return 0;
+}
