05db5af1e3bed97b50e7e458df00a1147444c11d
diyarani823 <diyarani132@gmail.com>
2025-12-09 21:06:38 +0500

    .

---
+++ Practice/Cont vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,count=0;
+	printf("enter a string");
+	scanf("%s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		char ch=str[i];
+		if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		count++;
+	
+	}
+	printf("%d",count);
+	}
